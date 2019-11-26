d1bc6689ffdaae5fc7da14301d0a65fe83198bc1
succcubbus <16743652+succcubbus@users.noreply.github.com>
2019-11-26 21:15:57 +0100

    give args unique names

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use clap::{App, Arg, Shell};
 
 fn main() {
+    let names = (b'a'..b'z')
+        .map(|b| b as char)
+        .flat_map(|char1| (b'a'..b'z').map(move |char2| format!("{}{}", char1, char2 as char)))
+        .collect::<Vec<_>>();
     let values = (0..128u8)
         .map(|i| i as char)
         .filter(|c| !c.is_ascii_control())
@@ -13,8 +17,8 @@ fn main() {
         .collect::<Vec<_>>();
 
     let mut app = App::new("clap-zsh-completions-repro");
-    for v in &values {
-        app = app.arg(Arg::with_name("a").possible_value(v));
+    for (v, n) in values.iter().zip(&names) {
+        app = app.arg(Arg::with_name(n).possible_value(v));
     }
     app.gen_completions_to(
         "clap-zsh-completions-repro",
